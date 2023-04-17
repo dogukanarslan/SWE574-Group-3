597591bbb019eea451a803273a74e3fcd61e7fbb
afize1 <115039188+afize1@users.noreply.github.com>
2023-04-17 21:27:33 +0300

    urls updated

--- app/feed/urls.py
+++ app/feed/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.urls import path, include
-from .views import CreateTextAnnotationView
+
 
 from rest_framework.routers import DefaultRouter
 from . import views
@@ -9,7 +9,7 @@ router = DefaultRouter()
 router.register("space", views.SpaceViewSet)
 router.register("label", views.LabelViewSet)
 router.register("post", views.PostViewSet)
-router.register("annotation/", CreateTextAnnotationView, basename="annotation")
+router.register("annotation", views.CreateTextAnnotationView, basename="annotation")
 
 
 app_name = "meal"
