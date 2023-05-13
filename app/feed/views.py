f58dabfc2e74c37e1b140ce62b383d18dc4eac2d
Mehmet Emre Berk <emreberk92@gmail.com>
2023-05-13 15:03:47 +0300

    Settings.py reverted back, create method in LabelViewSet added, qid in Label model added, some modificiations in posts and _post.html

--- app/feed/views.py
+++ app/feed/views.py
@@ -24,6 +24,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import textAnnotation, Post
 from django_filters.rest_framework import DjangoFilterBackend
+import re
+import random
 
 import requests
 from django.http import JsonResponse
@@ -508,6 +510,23 @@ class SpaceViewSet(viewsets.ModelViewSet):
 class LabelViewSet(viewsets.ModelViewSet):
     serializer_class = LabelSerializer
     queryset = Label.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        if not name:
+            return Response({'name': 'This field is required.'}, status=400)
+
+        # check if name is a valid Wikidata query ID
+        if re.match(r'^Q\d+$', name):
+            label = Label.objects.create(name=name)
+            serializer = LabelSerializer(label)
+            return Response(serializer.data, status=201)
+        else:
+            # generate random QID
+            qid = '{}'.format(random.randint(1, 1000000000))
+            label = Label.objects.create(name=name, qid=qid)
+            serializer = LabelSerializer(label)
+            return Response(serializer.data, status=201)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
