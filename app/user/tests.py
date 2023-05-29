8078a5a69aa55dc83d277953c381063522e5a600
Miray Iyidogan <miray.iyidogan@hotmail.com>
2023-05-29 22:28:31 +0300

    Post like count added and badge condition updated

--- app/user/tests.py
+++ app/user/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
-
-# Create your tests here.
+from feed.models import Post
 from .models import *
-from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth import authenticate
 
 class UserLoginTest(TestCase):
     def setUp(self):
@@ -40,3 +38,38 @@ class UserLoginTest(TestCase):
             "/user/login/", {"email": "john.doe@test.com", "password": "wrong"}
         )
         self.assertFalse(response.status_code == 302)
+class BadgeAssignTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name="John",
+            last_name="Doe",
+            email="john.doe@test.com",
+            username="john.doe@test.com",
+            password="1234qwer",
+        )
+        self.friend = Friends.objects.create(
+            owner = self.user,
+        )
+        user_2 = User.objects.create_user(
+            first_name="Miray",
+            last_name="Iyidogan",
+            email="miray@test.com",
+            username="miray@test.com",
+            password="1234qwer",
+        )
+        post = Post.objects.create(
+            title="Post 1",
+            description="Description",
+            #post_link = "https://github.com/dogukanarslan/SWE574-Group-3",
+            owner=self.user,
+            platform="Platform",
+        )
+
+        post.liked_by.add(user_2) 
+        self.friend.friend_list.set([user_2])
+        self.user.save()
+
+    def test_assign_badge(self):      
+        self.user.refresh_from_db()
+        badge = self.user.badge
+        self.assertEqual('1', badge.level)
