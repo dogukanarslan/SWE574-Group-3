dbec63c331e7e6947f06f5a950cb6aa1fe2a9f68
ilaydakurtboun <115426763+ilaydakurtboun@users.noreply.github.com>
2023-05-29 13:08:45 +0300

    added unit tests for moderator and member of space

--- app/feed/tests.py
+++ app/feed/tests.py
@@ -25,7 +25,41 @@ class SpaceModelTest(TestCase):
             username="john.doe@test.com",
             password="1234qwer",
         )
-        Space.objects.create(title="Space 1", description="Description", owner=user)
+
+        moderator = User.objects.create_user(
+            first_name="Joe",
+            last_name="Doe",
+            email="joe.doe@test.com",
+            username="joe.doe@test.com",
+            password="1234qwer",
+        )
+
+        member = User.objects.create_user(
+            first_name="Alex",
+            last_name="Davis",
+            email="alex.davis@test.com",
+            username="alex.davis@test.com",
+            password="1234qwer",
+        )
+        moderator_default = User.objects.create_user(
+            first_name="Mary",
+            last_name="Brown",
+            email="mary.brown@test.com",
+            username="mary.brown@test.com",
+            password="1234qwer",
+        )
+
+        member_default = User.objects.create_user(
+            first_name="Robert",
+            last_name="Williams",
+            email="robert.williams@test.com",
+            username="robert.williams@test.com",
+            password="1234qwer",
+        )
+
+        space_obj = Space.objects.create(title="Space 1", description="Description", owner=user)
+        space_obj.moderator.add(moderator_default)
+        space_obj.member.add(member_default)
 
     def test_name(self):
         space = Space.objects.get(id=1)
@@ -37,6 +71,30 @@ class SpaceModelTest(TestCase):
         user = User.objects.get(id=1)
         expected_owner = user
         self.assertEqual(space.owner, expected_owner)
+
+    def test_adding_moderator(self):
+        space = Space.objects.get(id=1)
+        moderator = User.objects.get(id=2)
+        space.moderator.add(moderator)
+        self.assertTrue(space.moderator.filter(id=moderator.id).exists())
+
+    def test_adding_member(self):
+        space = Space.objects.get(id=1)
+        member = User.objects.get(id=3)
+        space.member.add(member)
+        self.assertTrue(space.member.filter(id=member.id).exists())
+
+    def test_moderator(self):
+        space = Space.objects.get(id=1)
+        moderator = User.objects.get(id=4)
+        expected_moderator = moderator
+        self.assertEqual(space.moderator.filter(id=moderator.id).first(), expected_moderator)
+
+    def test_member(self):
+        space = Space.objects.get(id=1)
+        member = User.objects.get(id=5)
+        expected_member= member
+        self.assertEqual(space.member.filter(id=member.id).first(), expected_member)
 
 
 class PostModelTest(TestCase):
