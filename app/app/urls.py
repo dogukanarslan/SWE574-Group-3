c8bd880247a021e604c59434669652eeeae5fc1d
ilaydakurtboun <115426763+ilaydakurtboun@users.noreply.github.com>
2023-06-06 15:09:03 +0300

    bug fix for swagger url

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls import url
+
 #from feed.views import wikidata_search
 
 schema_view = get_schema_view(
@@ -36,17 +38,17 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path(
+    url(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    path(
+    url(
         r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path(
+    url(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
     path("admin/", admin.site.urls),
